0830907a46a35683577ee4ee6f7a455d8b2cfc9a
Amaury Diaz <amauryuh@gmail.com>
2019-08-03 21:18:27 -0400

    Timeline ready.

--- README.md
+++
@@ -1,2 +0,0 @@
-# cimacuba.github.io
-This is the recipit for the website's static content of the cima church in Cuba.
---
+++ precache-manifest.899bc8ba567682cda5597886a7d2677c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4732ffc44269bfc9c6ea2f066019ba07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fceef6e067e5f3bb32b2",
+    "url": "/static/css/2.d819a5bb.chunk.css"
+  },
+  {
+    "revision": "4e6ec1a7374cf44fec31",
+    "url": "/static/css/main.806df36b.chunk.css"
+  },
+  {
+    "revision": "fceef6e067e5f3bb32b2",
+    "url": "/static/js/2.9b7a72f9.chunk.js"
+  },
+  {
+    "revision": "4e6ec1a7374cf44fec31",
+    "url": "/static/js/main.2aaa568b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
